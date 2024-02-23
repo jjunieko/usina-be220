bfc4153ed5f3bae93a05b28e17a745960d8e5f0a
JoaoJuniorProg <juniorjtr123@gmail.com>
2024-02-23 15:50:38 -0300

    feat: add itens build web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCV3jYhbRMNhxO74-lJE1cJKcy-Rs6Cl6Q",
+    authDomain: "testeapps-36c93.firebaseapp.com",
+    projectId: "testeapps-36c93",
+    storageBucket: "testeapps-36c93.appspot.com",
+    messagingSenderId: "534436070729",
+    appId: "1:534436070729:web:13ca1dc23fbc667b1e4698",
+    measurementId: "G-SSNK5XHEHQ"
+  }
 };
